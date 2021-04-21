bc2119ed57bbffb3988a81778ede3062915fd1d9
myeonghunLee <dlaudgns2934@gmail.com>
2021-04-21 15:31:00 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://myeonghunLee:dnfntk1%23%23@boilerplate.6p5ug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -14,6 +22,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/register', (req, res) => {
+
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err,userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
